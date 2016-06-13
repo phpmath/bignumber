c10464994700a41c561a0431058943fc8c4ff3d8
Walter Tamboer <waltertamboer@users.noreply.github.com>
2016-06-13 15:11:30 +0200

    Feature/require ext bcmath (#1)
    * Added docblocks to the source folder
    
    * Added ext-bcmath to the list of requirements
    
    * Added a file header docblock to the test files
    
    * Moved the LICENSE file to LICENSE.md

--- src/Utils.php
+++ src/Utils.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * This file is part of phpmath/bignumber. (https://github.com/phpmath/bignumber)
+ *
+ * @link https://github.com/phpmath/bignumber for the canonical source repository
+ * @copyright Copyright (c) 2015-2016 phpmath. (https://github.com/phpmath/)
+ * @license https://raw.githubusercontent.com/phpmath/bignumber/master/LICENSE.md MIT
+ */
 
 namespace PHP\Math\BigNumber;
 
